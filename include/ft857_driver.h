f52c745e3261d960e16aa72ce2387f577fe31b7c
EA3HVJ <jplanellacosta@gmail.com>
2019-11-17 00:02:29 +0100

    Create ft857_driver.h

---
+++ include/ft857_driver.h
@@ -0,0 +1,50 @@
+/*
+ * Copyright (C) 2019 Joan Planella Costa
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * @file ft857_driver.h
+ * @author Joan Planella Costa - EA3HVJ
+ * @date 09/06/2019
+ * @version 0.9
+ * @brief Yaesu CAT interface for FT8x7 series
+ */
+
+#ifndef FT857_DRIVER_H
+#define FT857_DRIVER_H
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+#include "ft857_cat.h"
+#include "ft857_alignments.h"
+#include "ft857_global_status.h"
+#include "ft857_status_settings.h"
+#include "ft857_menu_settings.h"
+#include "ft857_mf_settings.h"
+#include "ft857_mchan.h"
+#include "ft857_misc_eeprom.h"
+#include "ft857_clone.h"
+#include "ft857_virtual.h"
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* FT857_DRIVER_H */
